6e4ed209efd33483fcc5060389deb5566021cfc3
xashleym <xashleymorrisonx@gmail.com>
2024-10-07 20:42:14 -0500

    assignment 0

---
+++ hello_gradescope.py
@@ -0,0 +1,10 @@
+# Name: Ashley Morrison
+# OSU Email: morria22@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: 0
+# Due Date: 10/7/24
+# Description: hello gradescope assignment submission
+
+
+def gradescope_test():
+    return 42
